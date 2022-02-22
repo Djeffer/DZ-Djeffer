48b2495f69b5cad65253cbb4230b31fe31fd5a68
Djeffer <eputanov@mail.ru>
2022-02-22 19:31:55 +0300

    home44

---
+++ OOP/home43/home43.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+class Parser:
+    html = ""
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_html(self):
+        req = requests.get(self.url).text
+        self.html = BeautifulSoup(req, 'lxml')
+
+    def parsing(self):
+        elements = self.html.find_all('div', class_='model-price-range')
+        prices = []
+        for element in elements:
+            pr1 = element.find_all('span')[0].text
+            price1 = re.sub(r'\D+', '', pr1)
+            pr2 = element.find_all('span')[1].text
+            price2 = re.sub(r'\D+', '', pr2)
+            if price2.isnumeric():
+                prices.append((int(price1) + int(price2)) / 2)
+            else:
+                prices.append(int(price1))
+        return prices
+
+    def run(self):
+        self.get_html()
+        return self.parsing()
+
+
+j = 0
+av = []
+for i in range(16):
+    pars = Parser(f'https://www.e-katalog.ru/list/206/{i}/')
+    av = pars.run()
+    j += 1
+
+
+print(f'На {j} страницах, средния цена: {round(sum(av) / len(av), 2)} rub')
+
